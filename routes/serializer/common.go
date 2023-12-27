ed995f6966e91db42739fda88f81ee7b2971abc7
linshengqian <linshengqian@outlook.com>
2023-12-27 09:02:33 +0800

    v2.01

--- routes/serializer/common.go
+++
@@ -1,9 +0,0 @@
-package serializer
-
-// Response 基础序列化器
-type Response struct {
-	Status int    `json:"status"`
-	Data   any    `json:"data"`
-	Msg    string `json:"msg"`
-	Error  string `json:"error"`
-}
